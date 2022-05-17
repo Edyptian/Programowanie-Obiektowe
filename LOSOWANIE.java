0d41211b6289e619164492b6a9dc39161adbbec2
Edyptian <101244666+Edyptian@users.noreply.github.com>
2022-05-17 21:41:46 +0200

    Add files via upload

---
+++ LOSOWANIE.java
@@ -0,0 +1,14 @@
+package com.lerestaurante.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LOSOWANIE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_losowanie);
+    }
+}
---
+++ User.java
@@ -0,0 +1,11 @@
+package com.lerestaurante.myapplication;
+
+public class User {
+    public String email;
+    public User(){
+
+    }
+    public User(String email){
+        this.email=email;
+    }
+}
